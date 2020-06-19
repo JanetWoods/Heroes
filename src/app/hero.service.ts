ebe204a51e24ae573970173522b87e2f141c04ce
Janet Woods <janetnwoods@gmail.com>
2020-06-19 13:29:04 -0400

    done routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,5 +17,10 @@ getHeroes(): Observable<Hero[]>{
   this.messageService.add('HeroService: fetched heroes');
   return of(HEROES);
 }
+getHero(id: number): Observable<Hero>{
+  //Todo - send message after fetching the hero
+  this.messageService.add(`HeroService: fetched hero id={id}`);
+  return of (HEROES.find(hero => hero.id === id));
+}
 
 }
